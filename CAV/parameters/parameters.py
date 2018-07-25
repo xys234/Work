a1a77611c7b460aa2d506008fa2d8aa17c5bfd60
xys234@gmail.com <wyin3@gatech.edu>
2018-07-24 23:22:23 -0400

    add parameters

--- CAV/parameters/parameters.py
+++ CAV/parameters/parameters.py
@@ -1,21 +1,18 @@
 
-
-
+from math import isclose
 
 model_parameters = {
 
     'first_year'                                        : 2018                      ,
     'current_freeway_vmt'                               : 39000000                  ,
     'current_arterial_vmt'                              : 45000000                  ,
-
     'city truck share_vmt'                              :	0.1                     ,
     'growth_rate'                                       :	0.015                   ,
-
-    'downtown_at'                                       :	0.1                     ,
-    'urban_at'                                          :	0.25                    ,
-    'suburban_business_at'                              :	0.1                     ,
-    'suburban_residential_at'                           :	0.4                     ,
-    'rural_at'                                          :	0.15                    ,
+    'downtown_AT'                                       :	0.1                     ,
+    'urban_AT'                                          :	0.25                    ,
+    'suburban_business_AT'                              :	0.1                     ,
+    'suburban_residential_AT'                           :	0.4                     ,
+    'rural_AT'                                          :	0.15                    ,
     'peak_period'                                       :   0.35                    ,
     'offpeak_period'                                    :   0.65                    ,
     'non_licensed_adult'                                :	0.1                     ,
@@ -47,11 +44,11 @@ model_parameters = {
     'suburban_business_ridesource_offset'               :	5                       ,
     'suburban_residential_ridesource_offset'            :	8                       ,
     'rural_ridesource_offset'                           :	17                      ,
-    'downtown_%_congested'                              :	0.8                     ,
-    'urban_%_congested'                                 :	0.5                     ,
-    'suburban_business_%_congested'                     :	0.4                     ,
-    'suburban_residential_%_congested'                  :	0.2                     ,
-    'rural_%_congested'                                 :	0.1                     ,
+    'downtown_percent_congested'                              :	0.8                     ,
+    'urban_percent_congested'                                 :	0.5                     ,
+    'suburban_business_percent_congested'                     :	0.4                     ,
+    'suburban_residential_percent_congested'                  :	0.2                     ,
+    'rural_percent_congested'                                 :	0.1                     ,
     'downtown_free_speed'                               :	20                      ,
     'urban_free_speed'                                  :	25                      ,
     'suburban_business_free_speed'                      :	30                      ,
@@ -60,40 +57,127 @@ model_parameters = {
     'non-licensed_adult_trip_spending'                  :	13.4                    ,
     'handicapped_and_elderly_trip_spending'             :	15.2                    ,
     'unaccompanied_children_trip_spending'              :	14.5                    ,
-    'travel_time_index_-_freeway_share'                 :	0.7                     ,
-    'freeway_planning_index_-_freeway_share'            :	0.3                     ,
-    'travel_time_index_-_arterial_share'                :	0.9                     ,
-    'freeway_planning_index_-_arterial_share'           :	0.1                     ,
-    'travel_time_index_-_area_type_share'               :	0.85                    ,
-    'freeway_planning_index_-_area_type_share'          :	0.15                    ,
-    'high_skill_jobs_-_winners'                         :	173220                  ,
-    'medium_skill_jobs_-_winners'                       :	2030                    ,
-    'low_skill_jobs_-_winners'                          :	15070                   ,
-    'high_skill_mean_salary_-_winners'                  :	108573                  ,
-    'medium_skill_mean_salary_-_winners'                :	57657                   ,
-    'low_skill_mean_salary_-_winners'                   :	31516                   ,
-    'high_skill_jobs_-_losers'                          :	75120                   ,
-    'medium_skill_jobs_-_losers'                        :	66030                   ,
-    'low_skill_jobs_-_losers'                           :	144710                  ,
-    'high_skill_mean_salary_-_losers'                   :	128117                  ,
-    'medium_skill_mean_salary_-_losers'                 :	60085                   ,
-    'low_skill_mean_salary_-_losers'                    :	31111                   ,
-    'high_skill_jobs_-_%_lost'                          :	0.416174121405751       ,
-    'medium_skill_jobs_-_%_lost'                        :	0.428381038921702       ,
-    'low_skill_jobs_-_%_lost'                           :	0.366242830488563
+    'travel_time_index_freeway_share'                 :	0.7                     ,
+    'freeway_planning_index_freeway_share'            :	0.3                     ,
+    'travel_time_index_arterial_share'                :	0.9                     ,
+    'freeway_planning_index_arterial_share'           :	0.1                     ,
+    'travel_time_index_area_type_share'               :	0.85                    ,
+    'freeway_planning_index_area_type_share'          :	0.15                    ,
+    'high_skill_jobs_winners'                         :	173220                  ,
+    'medium_skill_jobs_winners'                       :	2030                    ,
+    'low_skill_jobs_winners'                          :	15070                   ,
+    'high_skill_mean_salary_winners'                  :	108573                  ,
+    'medium_skill_mean_salary_winners'                :	57657                   ,
+    'low_skill_mean_salary_winners'                   :	31516                   ,
+    'high_skill_jobs_losers'                          :	75120                   ,
+    'medium_skill_jobs_losers'                        :	66030                   ,
+    'low_skill_jobs_losers'                           :	144710                  ,
+    'high_skill_mean_salary_losers'                   :	128117                  ,
+    'medium_skill_mean_salary_losers'                 :	60085                   ,
+    'low_skill_mean_salary_losers'                    :	31111                   ,
+    'high_skill_jobs_percent_lost'                          :	0.416174121405751,
+    'medium_skill_jobs_percent_lost'                        :	0.428381038921702,
+    'low_skill_jobs_percent_lost'                           :	0.366242830488563,
 
+    'VMT_fee'                                         :0.05,                 # per mile
+    'NOVMT_fee'                                       :0.10,
+    'pricing'                                         :0.10,
+
+    'percent_auto_drive': {
+                                'car1'  : {'freeway': 0.0, 'arterial': 0.0},
+                                'car2'  : {'freeway': 0.5, 'arterial': 0.5},
+                                'car3'  : {'freeway': 1.0, 'arterial': 1.0},
+                                'truck1': {'freeway': 0.0, 'arterial': 0.0},
+                                'truck2': {'freeway': 0.5, 'arterial': 0.5},
+                                'truck3': {'freeway': 1.0, 'arterial': 1.0},
+
+                           },
+
+    'auto_assistance':  {
+                             'standard': 0.0,
+                             'type1': 0.0,
+                             'type2': 0.3,
+                             'type3': 1.0
+    },
+
+    'cost_reduction_auto_truck': {
+
+        'midwest':      {'high':0.105, 'low':0.079},
+        'northeast':    {'high':0.105, 'low':0.079},
+        'southeast':    {'high':0.115, 'low':0.086},
+        'southwest':    {'high':0.101, 'low':0.076},
+        'west':         {'high':0.105, 'low':0.079},
+
+    }
 
 }
 
-area types = sum(b10:b14)
-time of day	=sum(b16:b17)
-winners	=sum(b47:b49)
-high	=b80/b33
-medium	=b81/b33
-low	=b82/b33
-losers	=sum(b51:b53)
-high	=b86/b33
-medium	=b87/b33
-low	=b88/b33
-current freeway vmt share	=b4/b6
-current arterial vmt share	=b5/b6
+
+def derive_parameters(param):
+    '''
+
+    :param model_parameter:
+    :return: update the model_parameter with derived parameters
+    '''
+
+    # Checks
+    assert isclose(sum([param['downtown_AT'], param['urban_AT'], param['suburban_business_AT'],
+                       param['suburban_residential_AT'], param['rural_AT']]), 1.0), \
+                       'Area type percentages must add up to 1.0'
+
+    assert isclose(sum([param['peak_period']+param['offpeak_period']]), 1.0), 'Period percentages must add up to 1.0'
+
+    # additional parameters
+    param['winners_high'] = param['high_skill_jobs_winners'] / param['current_employment']
+    param['winners_medium'] = param['medium_skill_jobs_winners'] / param['current_employment']
+    param['winners_low'] = param['low_skill_jobs_winners'] / param['current_employment']
+
+    param['losers_high'] = param['high_skill_jobs_losers'] / param['current_employment']
+    param['losers_medium'] = param['medium_skill_jobs_losers'] / param['current_employment']
+    param['losers_low'] = param['low_skill_jobs_losers'] / param['current_employment']
+
+    param['winners'] = param['winners_high'] + param['winners_medium'] + param['winners_low']
+    param['losers'] = param['losers_high'] + param['losers_medium'] + param['losers_low']
+
+    param['current_total_vmt'] = param['current_freeway_vmt'] + param['current_arterial_vmt']
+    param['current_freeway_vmt_share'] = param['current_freeway_vmt'] * 1.0 / param['current_total_vmt']
+    param['current_arterial_vmt_share'] = param['current_arterial_vmt'] * 1.0 / param['current_total_vmt']
+
+
+    # year weights
+    param['year_weights'] = {}
+    for t in ['car1', 'car2', 'car3', 'truck1', 'truck2', 'truck3']:
+        for f in ['freeway', 'arterial']:
+            if param['percent_auto_drive'][t][f] > 0:
+                if t in ('car1', 'car2', 'car3'):
+                    param['year_weights'][(t,f)] = \
+                        (1.0 - param['city truck share_vmt']) / param['percent_auto_drive'][t][f]
+                else:
+                    param['year_weights'][(t, f)] = \
+                        param['city truck share_vmt'] / param['percent_auto_drive'][t][f]
+            else:
+                param['year_weights'][(t, f)] = 0.0
+
+    return param
+
+
+def get_congestion_stats(model_parameters):
+    '''
+
+    :param model_parameters:
+    :return:
+    '''
+
+
+
+
+if __name__=='__main__':
+
+    model_parameters = derive_parameters(model_parameters)
+    for k, v in model_parameters.items():
+        if isinstance(v, str):
+            print(k.ljust(50) + ":{0}".format(v))
+        elif isinstance(v, (list, dict)):
+            print(k.ljust(50) + str(v))
+        else:
+            print(k.ljust(50) + "{0:.4f}".format(v))
