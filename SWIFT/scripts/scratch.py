757535f5157129458b4492d53b4f86e27c8a5585
yinw <xys234@gmail.com>
2018-12-02 18:00:43 -0500

    build index to veh trajectory

--- SWIFT/scripts/scratch.py
+++ SWIFT/scripts/scratch.py
@@ -1,25 +1,60 @@
-import numpy as np
+import os
 import textwrap
 
-class Celsius:
-    def __init__(self, temperature = 0):
-        self._temperature = temperature
-        print("created")
+# seeking position considers return as 2 chars
+# read position considers return as 1 char
+# todo: Read in the adjustment file; Scan the trip file;
 
-    def to_fahrenheit(self):
-        return (self.temperature * 1.8) + 32
 
-    @property
-    def temperature(self):
-        print("Getting value")
-        return self._temperature
+def build_vehicle_id_index(veh_trajectory_file):
+    veh_id_pos_index = {}
+    prev_pos, pos, veh_count, offset, prev_line = 0, 0, 0, 0, 0
+    with open(veh_trajectory_file, mode='r', buffering=10_000_000) as veh_file:
+        for i, line in enumerate(veh_file):
+            if line.startswith("Veh #"):
+                if veh_count == 0:
+                    prev_pos = pos
+                else:
+                    offset = pos - prev_pos - (i-prev_line)
+                    veh_id_pos_index[veh_id] = (prev_pos, offset)
+                    prev_pos = pos
+                prev_line = i
+                veh_count += 1
+                veh_id = int(line[:15].split()[2])
+            pos += len(line)+1
+            if i > 20:
+                break
+    return veh_id_pos_index
 
-    @temperature.setter
-    def temperature(self, value):
-        if value < -273:
-            raise ValueError("Temperature below -273 is not possible")
-        print("Setting value")
-        self._temperature = value
+
+def write_trajectories(veh_trajectory_file, vehicle_id_index, adj_list, output):
+    """
+
+    :param veh_trajectory_file:
+    :param vehicle_id_index:
+    :param adj_list: a list of vehicle ids. Negative id means deletion
+    :type list
+    :return:
+    """
+    VEHICLE_ID_FIELD_LENGTH =14
+    VEHICLE_FIELD_HEADER = "Veh #"
+    start_new_id = 10_000
+    ids = set()
+    with open(veh_trajectory_file, mode='r') as input:
+        with open(output, mode='w') as out:
+            for v in adj_list:
+                if v > 0:
+                    start_pos, offset = vehicle_id_index[v]
+                    input.seek(start_pos)
+                    record = input.read(offset)
+                    if v not in ids:
+                        ids.add(v)
+                        # add the original trajectory
+                        out.write(record)
+                    new_veh_id_field = VEHICLE_FIELD_HEADER + str(start_new_id).rjust(9)
+                    record = new_veh_id_field + record[VEHICLE_ID_FIELD_LENGTH:]
+                    out.write(record)
+                    start_new_id += 1
 
 
 def wrap_list(lst, items_per_line=5):
@@ -30,11 +65,33 @@ def wrap_list(lst, items_per_line=5):
         lines.append(line)
     return "" + ",\n ".join(lines) + ""
 
+
 if __name__ == '__main__':
 
-   import re
-   line = "TITLE\t\t\tConvertTrips"
-   line2 = line.replace("\t", "    ")
-   print(line2)
-   res = re.split(r"[\s]{2,}", line2)
-   print(res)
+    data_dir = r'C:\Projects\Repo\Work\SWIFT\data\Dynus_T'
+    veh_trajectory_file = 'VehTrajectory.dat'
+    # veh_trajectory_file = 'input.dat'
+    out_file = 'test.dat'
+    veh_trajectory_file = os.path.join(data_dir, veh_trajectory_file)
+    out_file = os.path.join(data_dir, out_file)
+
+    # with open(veh_trajectory_file, mode='r') as f:
+    #     pos = 0
+    #     for i, line in enumerate(f):
+    #         pos += len(line)
+    #         print("Line {0:d} = {1:d} chars".format(i+1, len(line)+1))
+    #         if i > 10:
+    #             break
+    #
+    with open(veh_trajectory_file, mode='r') as f:
+        with open(out_file, mode='w') as out:
+            f.seek(532)
+            data = f.read(357)
+            out.write(data)
+
+    veh_id_pos_index = build_vehicle_id_index(veh_trajectory_file)
+    adj_list = [
+        160, 160, -519, 403
+    ]
+    write_trajectories(veh_trajectory_file, veh_id_pos_index, adj_list, out_file)
+
